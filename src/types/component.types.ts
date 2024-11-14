9b417f1b51cc962faf90056e5e8a2d7e5b4c8352
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2024-11-14 16:43:40 +0530

    Fixed errors

--- src/types/component.types.ts
+++ src/types/component.types.ts
@@ -1,9 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export type HeroDownSectionComponentProps = {};
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export type FooterSectionComponentProps = {};
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export type GallerySectionComponentProps = {};
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export type HeroSectionComponentProps = {};
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export type IntroduceSectionComponentProps = {};
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export type MoreRoadsSectionComponentProps = {};
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export type FeaturesSectionComponentProps = {};
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export type NewRiderSectionComponentProps = {};
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export type StylingSectionComponentProps = {};
